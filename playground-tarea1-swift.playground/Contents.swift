c3132af2d6d9afaa560125945c29cdaf479c1fa7
olver <olver@olvers-Mac-mini.local>
2015-11-05 11:40:29 -0800

    añadiendo el playgorund al repositorio

---
+++ playground-tarea1-swift.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+for i in 0...100{
+    
+    if i%5==0{
+        print("\(i) Bingo!!!\n")
+    }
+    
+    if i%2==0{
+        print("\(i) es PAR!!!\n")
+    }else{
+        print("\(i) es Impar!!!\n")
+    }
+    
+    if i>=30 && i<=40{
+        print("\(i) Viva Swift!!!!\n")
+    }
+}
