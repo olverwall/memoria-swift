520ab590699dae857267c944fbc563bdd3731be1
olver <olver@olvers-Mac-mini.local>
2015-11-07 03:50:55 -0800

    evitando el número 0

--- playground-tarea1-swift.playground/Contents.swift
+++ playground-tarea1-swift.playground/Contents.swift
@@ -4,17 +4,20 @@ import UIKit
 
 for i in 0...100{
     
-    if i%5==0{
-        print("\(i) Bingo!!!\n")
-    }
+    if i != 0{
+        
+        if i%5==0{
+            print("\(i) Bingo!!!\n")
+        }
     
-    if i%2==0{
-        print("\(i) es PAR!!!\n")
-    }else{
-        print("\(i) es Impar!!!\n")
-    }
+        if i%2==0 {
+            print("\(i) es PAR!!!\n")
+        }else{
+            print("\(i) es Impar!!!\n")
+        }
     
-    if i>=30 && i<=40{
-        print("\(i) Viva Swift!!!!\n")
+        if i>=30 && i<=40{
+            print("\(i) Viva Swift!!!!\n")
+        }
     }
 }
