5289a4cbb8f49a432f0a104d9ea386f1e4aed6ce
olver <olver@olvers-Mac-mini.local>
2015-11-16 04:40:23 -0800

    velocimetro tarea 2 curso swift

---
+++ velocimetro.playground/Contents.swift
@@ -0,0 +1,63 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum Velocidades:Int{
+    case apagado = 0
+    case velocidadBaja = 20
+    case velocidadMedia = 50
+    case velocidadAlta = 120
+    
+    init(velocidadInicial : Velocidades){
+       self = velocidadInicial
+    }
+}
+
+class Auto{
+    var velocidad : Velocidades
+    
+    init(){
+        let aux = Velocidades.apagado
+        self.velocidad = Velocidades(velocidadInicial: aux);
+    }
+    
+    func cambioDeVelocidad() ->(actual : Int, velocidadEnCadena : String){
+        var act: Int = 0
+        var cadena : String = ""
+        
+        switch self.velocidad {
+        case .apagado:
+            act = velocidad.rawValue
+            cadena = "Apagado"
+            self.velocidad = Velocidades.velocidadBaja;
+            
+        case .velocidadBaja:
+            
+            act = velocidad.rawValue
+            cadena = "velocidadBaja"
+            self.velocidad = Velocidades.velocidadMedia
+            
+        case .velocidadMedia:
+            
+            act = velocidad.rawValue
+            cadena = "velocidadMedia"
+            self.velocidad = Velocidades.velocidadAlta
+            
+        case .velocidadAlta:
+            
+            act = velocidad.rawValue
+            cadena = "velocidadAlta"
+            self.velocidad = Velocidades.velocidadMedia
+        }
+        return (actual : act,velocidadEnCadena : cadena)
+        
+    }
+}
+// pruebas de la clase
+
+let coche = Auto()
+
+for i in 1...20{
+    var result = coche.cambioDeVelocidad()
+    print("\(i) \(result.actual) \(result.velocidadEnCadena).\n\n")
+}
